e4063df8ec680e46a4d7ea44a96db88eff642948
Estrella <estrella.verdiguelcn@udlap.mx>
2025-11-11 10:02:49 +0100

    initial conmmit

---
+++ WaterTracker/WaterTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerApp.swift
+//  WaterTracker
+//
+//  Created by Estrella Verdiguel on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
